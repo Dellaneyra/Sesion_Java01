64481faf93bb4b72443d2651d8315d798dc20ee0
Dellaneyra <dellaneyravazquez@gmail.com>
2023-10-05 22:37:43 -0600

    actualizacion java

--- POO/src/Persona/Usuario.java
+++ POO/src/Persona/Usuario.java
@@ -2,48 +2,43 @@ package Persona;
 
 public class Usuario {
 
-	
 	//Atributo
 	private String tipoUsuario;
 	private String username;
 	private String password;
-	
+
 	//Crear un nuevo usuario (constructor)
-	 public Usuario (String tipoUsuario, String username, String password) {
-		 this.tipoUsuario = tipoUsuario;
-		 this.username = username;
-		 this.password = password;
-	 }
-	
-	
+	public Usuario(String tipoUsuario, String username, String password) {
+		this.tipoUsuario = tipoUsuario;
+		this.username = username;
+		this.password = password;
+	}//constructor
+
 	//Recuperar contraseña (getter)
 	public String getPassword() {
-		//Si el usuario registrado coincide con el usuario que paso como parametro, entonces obtengo la contraseña
+		//si el usuario registrado coincide con el usuario que paso como paramétro, entonces obtengo la contraseña
 		return password;
-	}
-	
-	
-	//Cambiar contraseña (setter)
-	public void setPassword(String nuevoPassword) {
-		//Si la contraseña es diferente a una cadena vacia y es diferente a la contraseña anterior...
-		if(nuevoPassword !="" && nuevoPassword != password) {
-			//...entonces reemplazo la contraseña anterior
+	}//getter
+
+
+	//Cambiar contaseña (setter)
+	public  void setPassword(String nuevoPassword) {
+		//Si la nueva contraseña es diferente a una cadena vacía y es diferente a la contraseña anterior... 
+		if(nuevoPassword != "" && nuevoPassword != password) {
+			//.. entonces reemplazo la contraseña anterior
 			password = nuevoPassword;
-		} else {
-			System.out.println("Lo siento, no puedo cambiar la contraseña");
+			System.out.println("Contaseña cambiada correctamente");
+		}else {
+			System.out.println("Lo siento, no puedo cambiar la contaseña");
 		}
-	}
-	
-	
-	//Cambiar la contraseña de un objeto del tipo usuario
-	//Felipe.setpassword (donde la contraseña este vacía)
-	//Felipe.setpassword (donde la contraseña no este vacia pero sea igual a la anterioro)
-	//Felipe.setpassword (donde se cumplan las 2 condiciones)
-	
-	
-	
-	
-	
-	
-	
-}// class usuario
+	}//setter
+
+
+	//Cambiar la constraseña de un objeto del tipo usuario
+	//Felipe.setPassword (una donde la contrseña esté vacía)
+	//Felipe.setPassword (una donde la contraseña no esté vacía pero sea igual a la anterior)
+	//Felipe.password (se cumplan las dos condiciones)
+
+
+
+}//class
